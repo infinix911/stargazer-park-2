8e6d9f24785285bfaae9db7475e1ed40635678f3
jcbmilanes <jcbmilanes@gmail.com>
2025-08-09 19:32:47 +0800

    icon updates

--- src/views/auth/RegisterPage.vue
+++ src/views/auth/RegisterPage.vue
@@ -15,9 +15,7 @@
           <!-- Username Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-              </svg>
+              <User class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.username') }}</label>
             </div>
             <Input
@@ -33,9 +31,7 @@
           <!-- Password Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
-              </svg>
+              <Lock class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.password') }}</label>
             </div>
             <div class="relative">
@@ -51,13 +47,8 @@
                 @click="togglePassword"
                 class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600"
               >
-                <svg v-if="showPassword" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.878 9.878L3 3m6.878 6.878L21 21"></path>
-                </svg>
-                <svg v-else class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                </svg>
+                <EyeOff v-if="showPassword" class="w-5 h-5" />
+                <Eye v-else class="w-5 h-5" />
               </button>
             </div>
             <p v-if="errors.password" class="text-red-400 text-sm">{{ errors.password }}</p>
@@ -66,9 +57,7 @@
           <!-- Confirm Password Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
-              </svg>
+              <Lock class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.confirmPassword') }}</label>
             </div>
             <div class="relative">
@@ -84,13 +73,8 @@
                 @click="toggleConfirmPassword"
                 class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600"
               >
-                <svg v-if="showConfirmPassword" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.878 9.878L3 3m6.878 6.878L21 21"></path>
-                </svg>
-                <svg v-else class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                </svg>
+                <EyeOff v-if="showConfirmPassword" class="w-5 h-5" />
+                <Eye v-else class="w-5 h-5" />
               </button>
             </div>
             <p v-if="errors.confirmPassword" class="text-red-400 text-sm">{{ errors.confirmPassword }}</p>
@@ -99,9 +83,7 @@
           <!-- Name Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-              </svg>
+              <User class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.name') }}</label>
             </div>
             <Input
@@ -117,9 +99,7 @@
           <!-- Mobile Number Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-              </svg>
+              <Phone class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.mobileNumber') }}</label>
             </div>
             <div class="relative">
@@ -150,9 +130,7 @@
           <!-- Bank Name Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-              </svg>
+              <Building2 class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.bankName') }}</label>
             </div>
             <Select
@@ -174,9 +152,7 @@
           <!-- Bank Account Name Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-              </svg>
+              <User class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.bankAccountName') }}</label>
             </div>
             <Input
@@ -192,9 +168,7 @@
           <!-- Bank Account Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
-              </svg>
+              <CreditCard class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.bankAccount') }}</label>
             </div>
             <div class="relative">
@@ -223,9 +197,7 @@
           <!-- Referral Code Field -->
           <div class="space-y-2">
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path>
-              </svg>
+              <Tag class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.referralCode') }}</label>
             </div>
             <Input
@@ -247,9 +219,7 @@
             </div>
             
             <div class="flex items-center space-x-2">
-              <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
-              </svg>
+              <Lock class="w-5 h-5 text-gray-700" />
               <label class="text-gray-700 font-medium">{{ t('register.captcha') }}</label>
             </div>
             
@@ -268,9 +238,7 @@
                 size="icon"
                 class="bg-white border-gray-300 text-gray-700 hover:bg-gray-50"
               >
-                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
-                </svg>
+                <RefreshCw class="w-4 h-4" />
               </Button>
             </div>
             <p v-if="errors.captcha" class="text-red-400 text-sm">{{ errors.captcha }}</p>
@@ -308,6 +276,17 @@ import { useI18n } from 'vue-i18n'
 import { Input } from '../../components/ui/input'
 import Button from '../../components/ui/Button.vue'
 import Select from '../../components/ui/select.vue'
+import { 
+  User, 
+  Lock, 
+  Eye, 
+  EyeOff, 
+  Phone, 
+  Building2, 
+  CreditCard, 
+  Tag, 
+  RefreshCw 
+} from 'lucide-vue-next'
 
 const { t } = useI18n()
 
